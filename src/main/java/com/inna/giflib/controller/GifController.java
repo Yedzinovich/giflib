1485ea670e140c4adeadd32a71518a18ad661f7c
Inna <innayedzinovich@gmail.com>
2019-08-16 13:57:25 -0400

    Test GIF successfully appeared

---
+++ src/main/java/com/inna/giflib/controller/GifController.java
@@ -0,0 +1,13 @@
+package com.inna.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    public String listGifts(){
+        return "home";
+    }
+}
