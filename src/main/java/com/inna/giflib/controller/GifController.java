7ba0556e74e8dfa6f840d5d6e940e7660a475260
Inna <innayedzinovich@gmail.com>
2019-08-19 13:50:30 -0400

    Models added, controller features added

--- src/main/java/com/inna/giflib/controller/GifController.java
+++ src/main/java/com/inna/giflib/controller/GifController.java
@@ -1,13 +1,31 @@
 package com.inna.giflib.controller;
 
+import com.inna.giflib.data.GifRepository;
+import com.inna.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import sun.awt.image.GifImageDecoder;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifts(){
         return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        Gif gif = gifRepository.findByName(name);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
